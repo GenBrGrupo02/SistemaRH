1a9de784d54aa9905b499cebdb37a81f5f54c660
StPVieira <pedrohq.vieira168@gmail.com>
2024-12-03 11:05:34 -0300

    Classe Model Empregado criada

---
+++ src/main/java/com/generation/pi/sistemarh/model/empregado/Empregado.java
@@ -0,0 +1,78 @@
+package com.generation.pi.sistemarh.model.empregado;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "tb_empregado")
+public class Empregado {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O atributo nome é obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo nome deve conter no mínimo 05 e no máximo 100 caracteres.")
+	private String nome;
+	
+	@NotBlank(message = "O atributo cargo é obrigatório!")
+	@Size(min = 5, max = 100, message = "O atributo cargo deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String cargo;
+	
+	@NotBlank(message = "O atributo salário é obrigatório!")
+	private float salario;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("empresa")
+	private Empresa empresa;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public float getSalario() {
+		return salario;
+	}
+
+	public void setSalario(float salario) {
+		this.salario = salario;
+	}
+
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+
+}
