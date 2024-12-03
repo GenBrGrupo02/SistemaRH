f6180fef8c8accd64477dd11f2a4d91c15555c2e
Catharina Zagiacomo <catharina.zagiacomo@gmail.com>
2024-12-03 10:08:30 -0300

    Criação do Projeto
    Primeiro commit de criação do projeto base para sistema de RH

---
+++ src/main/java/com/generation/pi/sistemarh/SistemarhApplication.java
@@ -0,0 +1,13 @@
+package com.generation.pi.sistemarh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemarhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemarhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/pi/sistemarh/SistemarhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.pi.sistemarh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemarhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
