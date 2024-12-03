62cd6a7f043d592a860b0b1e558b13268dd21020
Hector Silva Santos <promotoraeliana@yahoo.com.br>
2024-12-03 12:09:56 -0300

    EmpresaController e Empresa Model criados.

--- src/main/java/com/generation/pi/sistemarh/controller/EmpresaController.java
+++ src/main/java/com/generation/pi/sistemarh/controller/EmpresaController.java
@@ -1,5 +1,23 @@
 package com.generation.pi.sistemarh.controller;
 
+import com.generation.pi.sistemarh.model.EmpresaModel;
+import com.generation.pi.sistemarh.repository.EmpresaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/empresas")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class EmpresaController {
 
+    @Autowired
+    private EmpregadoRepository empregadoRepository;
+
+
+   
 }
